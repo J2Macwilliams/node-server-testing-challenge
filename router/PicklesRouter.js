de13b497cac10909a3e6e50fa508ab656ab7a337
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-19 15:00:56 -0800

    Create boilerplate for endpoints.

---
+++ router/PicklesRouter.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+
+//Pull in knex helper models
+const dB = require('../model/PicklesDB');
+
+//Global GET
+router.get('/', (req, res) => {
+    
+});
+
+//GET by id
+router.get('/:id', (req, res) => {
+    
+});
+
+//POST
+router.post('/', (req, res) => {
+    
+});
+
+//DELETE by id
+router.delete('/:id', (req, res) => {
+    
+});
+
+module.exports = router;
