380f7e21e66c4e064719805ea82ea28318f00d90
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-19 13:19:52 -0800

    Remove server spec test. Redundant.

--- api/server.spec.js
+++
@@ -1,7 +0,0 @@
-describe("server.js", function() {
-    describe("environment", function() {
-      it("should set environment to testing", function() {
-        expect(process.env.DB_ENV).toBe("testing");
-      });
-    });
-});
