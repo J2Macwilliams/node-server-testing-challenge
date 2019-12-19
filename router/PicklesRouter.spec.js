da79ae14680587f787ca384e8444b373ebdadba5
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-19 13:47:21 -0800

    Write tests for POST.

--- router/PicklesRouter.spec.js
+++ router/PicklesRouter.spec.js
@@ -1,12 +1,42 @@
-// const request = require('supertest');
+const request = require('supertest');
 
-// const server = require('./server.js');
+const server = require('./server.js');
 
 describe('PickleRouter.js', function() {
+    // environment test---------------------------------------
 	describe('environment', function() {
 		it('should set environment to testing', function() {
 			expect(process.env.DB_ENV).toBe('testing');
 		});
     });
+    //POST test for JSON -------------------------------------
+	describe('POST /', function() {
+		it('should return a JSON', function() {
+			return request(server)
+				.post('/')
+				.then(res => {
+					expect(res.type).toMatch(/json/i);
+				});
+        });
+    // POST test for status code and OBJECT----------------------
+		it('should return posted Object', function() {
+			return request(server)
+				.post('/')
+				.send('name = spicy pickle')
+				.expect(function(res) {
+					res.body.id = 'some fixed id';
+					res.body.name = res.body.name.toLowerCase();
+				})
+				.expect(
+					201,
+					{
+						id: 'some fixed id',
+						name: 'spicy pickle'
+					},
+					done
+				);
+		});
+    });
+    
     
 });
