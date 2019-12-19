42a930536d37dffef795271e61d286d9b1104aab
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-19 13:09:31 -0800

    Created server test and failed.

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,3 +1,7 @@
-const request = require("supertest");
-
-const server = require("./server.js");
+describe("server.js", function() {
+    describe("environment", function() {
+      it("should set environment to testing", function() {
+        expect(process.env.DB_ENV).toBe("testing");
+      });
+    });
+});
