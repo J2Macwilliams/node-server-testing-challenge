b43050bedc5e31b8eaaaf7034ea349e61ab74ed8
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-19 14:50:01 -0800

    Move PORT to .env file.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 5006;
+const port = process.env.PORT;
 
 server.listen(port, () => 
 console.log(`** Running on port ${port} **`));
